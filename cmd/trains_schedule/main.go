298e0f03707395c13ec748cbf7db6db185ba86e1
Alexander Lipatov <tellmeac@gmail.com>
2023-06-17 23:20:40 +0700

    trains schedule problem

---
+++ cmd/trains_schedule/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"time"
+)
+
+var (
+	reader = bufio.NewReader(os.Stdin)
+	writer = bufio.NewWriter(os.Stdout)
+)
+
+func main() {
+	var n int
+
+	_, _ = fmt.Fscanf(reader, "%d\n", &n)
+
+	var rawTs string
+	stamps := make([]int, 0, n)
+	for ; n > 0; n-- {
+		_, _ = fmt.Fscanf(reader, "%s ", &rawTs)
+		st, _ := time.Parse("15:04", rawTs)
+		stamps = append(stamps, st.Minute()+st.Hour()*60)
+	}
+
+	rotate := 24 * 60
+	if len(stamps) == 1 {
+		_, _ = fmt.Fprintf(writer, "%d\n", rotate)
+		return
+	}
+
+	sort.Slice(stamps, func(i, j int) bool {
+		return stamps[i] < stamps[j]
+	})
+
+	minDiff := math.Abs(float64(stamps[1] - stamps[0]))
+	for ind := 1; ind < len(stamps); ind++ {
+		minDiff = min(minDiff, math.Abs(float64(stamps[ind]-stamps[ind-1])))
+	}
+
+	// 00 00 to 23 59 case
+	cycleDiff := math.Abs(float64(stamps[len(stamps)-1] - stamps[0] - rotate))
+	minDiff = min(minDiff, cycleDiff)
+
+	_, _ = fmt.Fprintf(writer, "%d\n", int(minDiff))
+
+	_ = writer.Flush()
+}
+
+func min(a, b float64) float64 {
+	if a > b {
+		return b
+	}
+	return a
+}
